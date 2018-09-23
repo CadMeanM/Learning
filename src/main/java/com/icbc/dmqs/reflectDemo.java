388bb5b3b29a637d735e4b7d6c903f4d40e01126
CadMeanM <davidboyhz@163.com>
2018-09-23 16:18:51 +0800

    Delete reflectDemo.java

--- src/main/java/com/icbc/dmqs/reflectDemo.java
+++
@@ -1,9 +0,0 @@
-package com.icbc.dmqs;
-
-public class reflectDemo {
-    public static void main(String[] args){
-        System.out.println(reflectDemo.class.getName());
-        System.out.println("\n");
-        System.currentTimeMillis();
-    }
-}
