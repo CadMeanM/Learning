e1ce2c3774f36c30f88adfe1f4f458f6a016a7e4
Cadmean <hzcadmean@hotmail.com>
2018-09-23 17:12:09 +0800

    1. Reflect-Methods

--- src/main/java/com/icbc/dmqs/Methods/MethodInvoke.java
+++ src/main/java/com/icbc/dmqs/Methods/MethodInvoke.java
@@ -1,16 +1,30 @@
 package com.icbc.dmqs.Methods;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class MethodInvoke {
+    public String tag="1";
     public static void main(String[] args) throws Exception {
         Class<?> clazz = Class.forName("com.icbc.dmqs.Methods.MethodsReflect");
+
+        MethodInvoke obj = (MethodInvoke) Class.forName("com.icbc.dmqs.Methods.MethodInvoke").newInstance();
+
         // 调用TestReflect类中的reflect1方法
-        Method method = clazz.getMethod("getAMethods", String.class);
-        Method[] meths = (Method[]) method.invoke(clazz.newInstance(),"com.icbc.dmqs.Methods.MethodsReflect");
+        Method method = clazz.getMethod("getDMethods", String.class);
+        Method[] meths = (Method[]) method.invoke(clazz.newInstance(),"com.icbc.dmqs.Methods.MethodInvoke");
         for (Method m: meths){
             System.out.println(m.getName());
         }
+        System.out.println("Set tag to "+obj.tag+" before change it");
+
+        Field field = Class.forName("com.icbc.dmqs.Methods.MethodInvoke").getDeclaredField("tag");
+        field.setAccessible(true);
+        field.set(obj,"2");
+
+        System.out.println("Set tag to "+obj.tag+" after change it");
+
 //        // Java 反射机制 - 调用某个类的方法1.
 //        // 调用TestReflect的reflect2方法
 //        method = clazz.getMethod("reflect2", int.class, String.class);
