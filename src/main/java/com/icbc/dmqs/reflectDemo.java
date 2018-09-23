34abdbe0b6fdd3c27987c7317eed73af0f68ca8d
Cadmean <hzcadmean@hotmail.com>
2018-09-23 15:42:09 +0800

    initialize project

--- src/main/java/com/icbc/dmqs/reflectDemo.java
+++ src/main/java/com/icbc/dmqs/reflectDemo.java
@@ -3,5 +3,6 @@ package com.icbc.dmqs;
 public class reflectDemo {
     public static void main(String[] args){
         System.out.println(reflectDemo.class.getName());
+        System.out.println("\n");
     }
 }
