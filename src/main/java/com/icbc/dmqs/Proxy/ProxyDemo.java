ddffbc12dad4dd61f842dff3f030617ffeaf6b4c
Cadmean <hzcadmean@hotmail.com>
2018-09-23 18:11:07 +0800

    Reflect-proxy

--- src/main/java/com/icbc/dmqs/Proxy/ProxyDemo.java
+++ src/main/java/com/icbc/dmqs/Proxy/ProxyDemo.java
@@ -8,18 +8,28 @@ interface Subject {
 }
 // 定义真实项目
 class RealSubject implements Subject {
+    String name;
+    RealSubject(){
+
+    }
+
+    RealSubject(String name){
+        this.name=name;
+    }
     public String say(String name, int age) {
-        return name + "  " + age;
+        String n = this.name == null? name:this.name;
+        return n + "  " + age;
     }
 }
 class MyInvocationHandler implements InvocationHandler {
     private Object obj = null;
+    private int tag = 0;
     public Object bind(Object obj) {
         this.obj = obj;
         return Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), this);
     }
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.print("进入invoke方法: ");
+        System.out.print("进入invoke方法第"+(++tag)+"次： ");
         Object temp = method.invoke(this.obj, args);
         return temp;
     }
@@ -30,7 +40,10 @@ public class ProxyDemo {
         Subject sub = (Subject) demo.bind(new RealSubject());
         System.out.println(sub.say("Rollen", 20));
 
+        sub = (Subject) demo.bind(new RealSubject("Tylon"));
+        System.out.println(sub.say("Rollen", 20));
+
         Subject testSub = new RealSubject();
-        System.out.println(testSub.say("Rollen2",21));
+        System.out.println(testSub.say("Byson",21));
     }
 }
---
+++ src/main/java/com/icbc/dmqs/Proxy/TicketProxyDemo.java
@@ -0,0 +1,55 @@
+package com.icbc.dmqs.Proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+interface Seller{
+    public Integer getPrice();
+}
+//真实卖家
+class RealSeller implements Seller{
+
+    public Integer getPrice() {
+        return 20;
+    }
+}
+//黑心卖家
+class FakeSeller implements Seller{
+
+    public Integer getPrice() {
+        return 40;
+    }
+}
+
+//中间商赚差价
+class TicketProxy implements InvocationHandler {
+
+    private Object obj = null;
+    private int fee;
+
+    TicketProxy(){
+        this.fee=10;
+    }
+
+    public Object bind(Object obj) {
+        this.obj = obj;
+        return Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), this);
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object price = method.invoke(this.obj,args);
+        int i = (Integer)price + fee;
+        return i;
+    }
+}
+public class TicketProxyDemo{
+    public static void main(String args[]){
+        TicketProxy tp = new TicketProxy();
+        Seller seller = (Seller) tp.bind(new RealSeller());
+        System.out.println("真实卖家："+new RealSeller().getPrice());
+        System.out.println("中间商赚个辛苦费："+seller.getPrice());
+        System.out.println("黑心卖家："+new FakeSeller().getPrice());
+        System.out.println("中间商和黑心卖家："+((Seller)tp.bind(new FakeSeller())).getPrice());
+    }
+}
